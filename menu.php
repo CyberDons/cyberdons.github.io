e938c657a9d7a2e3f0adc1ffeaa43cd02d225e68
CyberDons <corobotics3133@gmail.com>
2016-01-31 21:41:39 -0700

    Revert "Revert "upate""
    This reverts commit ae1ad5dc06cecf31aa2b39ccd118c1533c49c0b0.

---
+++ menu.php
@@ -0,0 +1,8 @@
+<ul class="navButtons">
+<li class="button"><a href="index.php">Home</a></li>
+<li class="button"><a href="team_info.php">Team Info</a></li>
+<li class="button"><a href="first.php">What is FIRST</a></li>
+<li class="button"><a href="competitions.php">Competitions</a></li>
+<li class="button"><a href="gallery.php">Gallery</a></li>
+<li class="button"><a href="contact.php">Contact US</a></li>
+</ul>
