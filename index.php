8b49b36939082b447508a54f855fa8ff7b81140c
freaksdesign <drivegt500@aol.com>
2016-01-31 21:39:40 -0700

    upate

---
+++ index.php
@@ -0,0 +1,11 @@
+< ?php include 'header.php'; ?>
+
+
+<div class="content">
+<div class="contentHead">Header Place Holder</div>
+<div class="contentText">
+	Place Holder
+</div>
+</div>
+
+< ?php include 'footer.php'; ?>
